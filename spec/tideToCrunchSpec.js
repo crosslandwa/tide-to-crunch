11d8305539dbe28e95bdf82859c56258804663aa
will.crossland <crossland.w.a@gmail.com>
2017-10-20 00:31:04 +0100

    Renaming

--- spec/tideToCrunchSpec.js
+++ spec/tideToCrunchSpec.js
@@ -1,23 +1,23 @@
-const { convert } = require('../src/index.js')
+const { convertToCrunchJson } = require('../src/index.js')
 
 describe('tide-to-crunch', () => {
   it('plucks "Date" values from the Tide CSV and converts them to dd/mm/yyyy format', () => {
     const csv = '"Date"\n"2017-10-19"\n"2017-10-18"'
-    result = convert(csv)
+    result = convertToCrunchJson(csv)
     expect(result[0].Date).toEqual('19/10/2017')
     expect(result[1].Date).toEqual('18/10/2017')
   })
 
   it('plucks "Transaction description" values from the Tide CSV', () => {
     const csv = '"Transaction description"\n"ref 1"\n"ref 2"'
-    result = convert(csv)
+    result = convertToCrunchJson(csv)
     expect(result[0]['Transaction description']).toEqual('ref 1')
     expect(result[1]['Transaction description']).toEqual('ref 2')
   })
 
   it('plucks "Amount" values from the Tide CSV and converts them to 2d.p. floats', () => {
     const csv = '"Amount"\n"123.00"\n"321.50"'
-    result = convert(csv)
+    result = convertToCrunchJson(csv)
     expect(result[0].Amount).toEqual(123.00)
     expect(result[1].Amount).toEqual(321.50)
   })
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-function convert(csvString) {
-  const tideKeys = parseLine(csvString.split('\n')[0])
-  const dataRows = csvString.split('\n').slice(1).map(parseLine)
+function convertToCrunchJson(csvString) {
+  const csvLines = csvString.split('\n')
+  const tideKeys = parseCsvLine(csvLines[0])
+  const dataRows = csvLines.slice(1).map(parseCsvLine)
 
   const tideJson = dataRows.map(row => row.reduce((acc, it, index) => {
     acc[tideKeys[index]] = it
@@ -14,12 +15,12 @@ function convert(csvString) {
   }))
 }
 
-function parseLine (raw) {
+function parseCsvLine (raw) {
   return raw.split(',').map(key => key.trim().replace(/"/g, ''))
 }
 
 const toDDMMYYYY = date => `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
 
 module.exports = {
-  convert
+  convertToCrunchJson
 }
