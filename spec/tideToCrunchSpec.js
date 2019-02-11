cfd09c93e16dc71c8719a16e4e9e5518757e94da
will.crossland <4037089+crosslandwa@users.noreply.github.com>
2019-02-11 22:36:53 +0000

    Sort transactions, latest first

--- spec/tideToCrunchSpec.js
+++ spec/tideToCrunchSpec.js
@@ -1,30 +1,48 @@
 const { convertToCrunchJson } = require('../src/index.js')
 
+const asJsObject = csv => {
+  const withoutDoubleQuotes = csv.replace(/"/g, '')
+  const [ headers, ...data ] = withoutDoubleQuotes.split('\n')
+  const splitByComma = x => x.split(',').map(x => x.trim())
+
+  return data.map(splitByComma).reduce((acc, line) => acc.concat(
+    splitByComma(headers).reduce((acc, key, i) => ({ ...acc, [key]: line[i] }), {})
+  ), [])
+}
+
 describe('tide-to-crunch', () => {
   it('plucks "Date" values from the Tide CSV and converts them to dd/mm/yyyy format', () => {
     const csv = '"Date"\n"2017-10-19"\n"2017-10-18"'
-    result = convertToCrunchJson(csv)
+    const result = convertToCrunchJson(asJsObject(csv))
     expect(result[0].Date).toEqual('19/10/2017')
     expect(result[1].Date).toEqual('18/10/2017')
   })
 
   it('plucks "Transaction description" values from the Tide CSV', () => {
     const csv = '"Transaction description"\n"ref 1"\n"ref 2"'
-    result = convertToCrunchJson(csv)
+    const result = convertToCrunchJson(asJsObject(csv))
     expect(result[0]['Transaction description']).toEqual('ref 1')
     expect(result[1]['Transaction description']).toEqual('ref 2')
   })
 
   it('plucks "Amount" values from the Tide CSV and converts them to 2d.p. floats', () => {
     const csv = '"Amount"\n"123.00"\n"321.50"'
-    result = convertToCrunchJson(csv)
+    const result = convertToCrunchJson(asJsObject(csv))
     expect(result[0].Amount).toEqual(123.00)
     expect(result[1].Amount).toEqual(321.50)
   })
 
+  it('sorts transactions by date order, most recent first', () => {
+    const csv = '"Amount","Timestamp"\n"3.00","2019-02-01 08:00:40"\n"1.00","2019-02-11 21:47:37"\n"2.00","2019-02-05 10:59:55"'
+    const result = convertToCrunchJson(asJsObject(csv))
+    expect(result[0].Amount).toEqual(1.00)
+    expect(result[1].Amount).toEqual(2.00)
+    expect(result[2].Amount).toEqual(3.00)
+  })
+
   it('takes a final balance and calculates the balance after each transaction', () => {
     const csv = '"Amount"\n"500.00"\n"-200.00"\n"1000.00'
-    result = convertToCrunchJson(csv, 10000)
+    const result = convertToCrunchJson(asJsObject(csv), 10000)
     expect(result[0].Amount).toEqual(500.00)
     expect(result[0].Balance).toEqual(10000.00)
 
@@ -34,5 +52,4 @@ describe('tide-to-crunch', () => {
     expect(result[2].Amount).toEqual(1000.00)
     expect(result[2].Balance).toEqual(9700.00)
   })
-
 })
--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ const { fromFilePath } = require('csvjsified')
 const toPence = pounds => Math.round(pounds * 100)
 const toPounds = pence => pence / 100
 
-function convertToCrunchJson(tideJson, finalBalance = 0) {
+function convertToCrunchJson (tideJson, finalBalance = 0) {
   return addBalance(
-    tideJson.map(tideRow => ({
+    sortTransactionsMostRecentFirst(tideJson).map(tideRow => ({
       Amount: parseFloat(parseFloat(tideRow.Amount).toFixed(2)),
       Date: toDDMMYYYY(new Date(tideRow.Date)),
       'Transaction description': tideRow['Transaction description']
@@ -14,9 +14,13 @@ function convertToCrunchJson(tideJson, finalBalance = 0) {
   )
 }
 
+const sortTransactionsMostRecentFirst = tideJson => [...tideJson].sort(
+  (a, b) => (a.Timestamp > b.Timestamp) ? -1 : ((b.Timestamp > a.Timestamp) ? 1 : 0)
+)
+
 function addBalance (payments, finalBalance) {
-  const balances = payments.reduce((acc, payment) =>
-    acc.concat(acc.slice(-1) - toPence(payment.Amount)),
+  const balances = payments.reduce(
+    (acc, payment) => acc.concat(acc.slice(-1) - toPence(payment.Amount)),
     [toPence(finalBalance)]
   )
   return payments.map((payment, index) => Object.assign({}, payment, { Balance: toPounds(balances[index]) }))
@@ -24,19 +28,19 @@ function addBalance (payments, finalBalance) {
 
 const toDDMMYYYY = date => `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
 
-function jsonToCsv(transactions) {
+function jsonToCsv (transactions) {
   const columns = ['Date', 'Transaction description', 'Amount', 'Balance']
   return [printCsvLine(columns)].concat(
     transactions
       .reduce((acc, transaction) => acc.concat([columns.map(column => transaction[column])]), [])
       .map(printCsvLine)
-    ).join('\n')
+  ).join('\n')
 }
 
 const printCsvLine = data => `"${data.join('","')}"`
 
 const writeToFile = outputFilename => data => new Promise(
-  (reject, resolve) => fs.writeFile(outputFilename, data, err => err ? reject(err) : resolve(data))
+  (resolve, reject) => fs.writeFile(outputFilename, data, err => err ? reject(err) : resolve(data))
 )
 
 module.exports = {
